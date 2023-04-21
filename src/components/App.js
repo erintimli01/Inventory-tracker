044337c753da653580983fd9877d637ec0c3e342
Erin Timlin <erintimlin@gmail.com>
2023-04-21 14:37:57 -0400

    update for keg control

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Header from "./Header";
+import KegControl from "./KegControl";
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App(){
   return (
     <React.Fragment>
-      <h1>Pierre's Brewery</h1>
-
-      <hr/>
+      <Container>
+        <Header />
+        <KegControl />
+      </Container>
     </React.Fragment>
   );
 }
