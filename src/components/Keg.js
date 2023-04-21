edeb2b9b351664253a58af6bf63eb99fa2c986ff
Erin Timlin <erintimlin@gmail.com>
2023-04-21 16:20:41 -0400

    add card for styling, change keg function to pass props, bottomsup WIP

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-// import Toast from 'react-bootstrap/Toast';
+import Toast from 'react-bootstrap/Toast';
 
-
-function Keg({ name, brand, price, kegId }) {
+function Keg(props){
+// function Keg({ name, brand, price, id }) {
   const [pintCount, setPintCount] = useState(124);
 
   const bottomsUp = () => {
@@ -14,15 +14,23 @@ function Keg({ name, brand, price, kegId }) {
 
   return (
     <div>
+            <Toast className="mb-4">
+      <Toast.Header closeButton={false}>
+        <div onClick={() => props.whenKegClicked(props.id)}>
       <ul>
-        <li>Name: {name}</li>
-        <li>Brand: {brand}</li>
-        <li>Price: ${price}</li>
-        <li>Remaining Pints: {pintCount}</li>
+        <li>Name: {props.name}</li>
+        <li>Brand: {props.brand}</li>
+        <li>Price: ${props.price}</li>
+        <li>Remaining Pints: {props.pintCount}</li>
       </ul>
-      <button onClick={bottomsUp} disabled={pintCount === 0}>
+      </div>
+      </Toast.Header>
+            {/* <Toast.Body>{props.desc}</Toast.Body> */}
+        </Toast>
+      {/* <button onClick={bottomsUp} disabled={pintCount === 0}>
         Bottoms up!
-      </button>
+      </button>  */}
+
     </div>
   );
 }
@@ -30,7 +38,8 @@ Keg.propTypes = {
   name: PropTypes.string.isRequired,
   brand: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
-  kegId: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  whenKegClicked: PropTypes.func
 };
 
 export default Keg;
