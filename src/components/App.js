3e4f579d6bad1a960b9ca867c1c16ced9adf6c8d
Erin Timlin <erintimlin@gmail.com>
2023-04-21 11:37:35 -0400

    update name

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App(){
+  return (
+    <React.Fragment>
+      <h1>Pierre's Beanery</h1>
+
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default App;
