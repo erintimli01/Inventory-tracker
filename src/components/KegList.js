20f074109528e2b872e3c1d695a3c8d781649e3d
Erin Timlin <erintimlin@gmail.com>
2023-04-21 13:10:42 -0400

    add keglist

---
+++ src/components/KegList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Keg from "./Keg";
+import propTypes from 'prop-types';
+
+function KegList(props) {
+  const { kegList } = props;
+
+  return (
+    <div>
+      <h2>Pierre's Brews</h2>
+      {kegList.map((keg) => (
+        <Keg
+        name={keg.name}
+        brand={keg.brand}
+        price={keg.price}
+        kegId={keg.id}
+        key={keg.id}
+        />
+      ))}
+    </div>
+  );
+}
+
+KegList.propTypes = {
+  kegList: propTypes.arrayOf(
+    propTypes.shape({
+      name: propTypes.string.isRequired,
+      brand: propTypes.string.isRequired,
+      price: propTypes.number.isRequired,
+      id: propTypes.string.isRequired,
+    })
+  ),
+};
+
+export default KegList;
