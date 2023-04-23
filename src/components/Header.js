98b8b6c38c9ecc600b14d6c9c2195e2f09463e1c
Erin Timlin <erintimlin@gmail.com>
2023-04-23 19:07:33 -0400

    add header image and style cards

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
 import React from "react";
-import DonutsBeer1 from "./../img/DonutsBeer1.jpg"
+import DonutsBeer4 from "./../img/DonutsBeer4.jpg"
 
-function Header() {
+function Header(){
+  const headerStyles = {
+    textAlign: "left",
+    padding: 30,
+    margin: 5,
+    backgroundImage:`url(${DonutsBeer4})`,
+    backgroundSize: "100%",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center top 30%",
+    color: "black",
+    boxShadow: "0 2px 10px rgba(0,0,0,0.6)"
+  }
+
+
   return(
+
+    <React.Fragment>
+      <div style={headerStyles}>
       <h1>Pierre's Brewery</h1>
+      {/* <h5>Keep track of the birds you spot!</h5> */}
+      </div>
+      <br />
+    </React.Fragment>
+
   );
 }
 
